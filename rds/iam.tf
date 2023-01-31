1497cd94474990cf9f4657df32b29cd281d4707b
Jayakishore Roy <jayakishore@datastream.org>
2023-01-31 15:03:43 -0500

    feat: enabling delivery status logging for rds SNS topic

--- rds/iam.tf
+++ rds/iam.tf
@@ -20,14 +20,52 @@ resource "aws_iam_role" "monitoring" {
 POLICY
 
   tags = merge(
-  local.tags,
-  {
-    Name = "${local.identifier} Monitoring"
-  }
+    local.tags,
+    {
+      Name = "${local.identifier} Monitoring"
+    }
   )
 }
 
 resource "aws_iam_role_policy_attachment" "monitoring" {
   role       = aws_iam_role.monitoring.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
+}
+
+resource "aws_iam_role" "rds-sns" {
+  name               = "${local.name}-rds-sns-role"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.rds-sns.json
+}
+
+data "aws_iam_policy_document" "rds-sns" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["sns.${data.aws_partition.current.dns_suffix}"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy" "rds-sns-feedback-role-policy" {
+  name   = "${local.identifier}-rds-sns-feedback-role-policy"
+  role   = aws_iam_role.rds-sns.id
+  policy = data.aws_iam_policy_document.rds-sns-feedback-role-policy.json
+}
+
+data "aws_iam_policy_document" "rds-sns-feedback-role-policy" {
+  statement {
+    effect  = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:PutMetricFilter",
+      "logs:PutRetentionPolicy"
+    ]
+    resources = ["*"]
+  }
 }
